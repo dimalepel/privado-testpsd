dc76fad5c3ce1cb489b16b22b402ae1288c46677
dimalepel <dimalepel@mail.ru>
2021-07-08 02:00:53 +0300

    finish desktop layout page

---
+++ index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Company name</title>
+  <link rel="stylesheet" href="styles/normalize.css">
+  <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+  <header class="page-header">
+    <div class="container page-header__container">
+      <a class="logo page-header__logo" href="/">
+        <img class="logo__picture" src="images/pic-logo.png" alt="">
+      </a>
+      <nav class="navigation">
+        <ul class="main-menu main-menu--horisontal">
+          <li class="main-menu__item">
+            <a class="main-menu__link" href="#">Home</a>
+          </li>
+          <li class="main-menu__item">
+            <a class="main-menu__link" href="#">About Us</a>
+          </li>
+          <li class="main-menu__item">
+            <a class="main-menu__link" href="#">Software</a>
+          </li>
+          <li class="main-menu__item">
+            <a class="main-menu__link" href="#">Pricing</a>
+          </li>
+        </ul>
+        <a class="button page-header__button" href="#">Get Started</a>
+      </nav>
+    </div>
+  </header>
+
+  <main class="page-content">
+    <section class="promo">
+      <div class="container">
+        <h1 class="promo__title">What is Lorem Ipsum?</h1>
+        <p class="promo__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <a class="button promo__button" href="#">Button</a>
+      </div>
+    </section>
+    <section class="advantages">
+      <h2 class="visually-hidden">Advantages</h2>
+      <ul class="container advantages__list">
+        <li class="advantages__item">Contrary to popular belief, Lorem <br>Ipsum is not simply random text</li>
+        <li class="advantages__item">Lorem ipsum dolor sit amet, <br>consectetur adipiscing elit</li>
+        <li class="advantages__item">The standard chunk of Lorem Ipsum <br>used since the 1500s is reproduced</li>
+      </ul>
+    </section>
+    <article class="container content">
+      <h2>Where does it come from?</h2>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+      <h2>Where can I get some?</h2>
+      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+    </article>
+    <section class="discount">
+      <div class="container discount__container">
+        <h2 class="discount__title">Discounts</h2>
+        <ul class="discount__list">
+          <li class="discount-item discount-item--true discount__item">
+            <div class="discount-item__header">
+              <h3 class="discount-item__title">Hosting1</h3>
+              <p class="discount-item__days">XXXX Days Retention - U.S. + E.U. Servers</p>
+              <a class="button discount-item__more" href="#">Button</a>
+            </div>
+            <ul class="discount-item__advantages">
+              <li class="discount-item__advantages-item discount-item__advantages-item--price">$x.xx/mo for 1yr</li>
+              <li class="discount-item__advantages-item discount-item__advantages-item--download">Unlimited downloads</li>
+              <li class="discount-item__advantages-item discount-item__advantages-item--speed">Unlimited speed</li>
+            </ul>
+            <ul class="discount-item__detail">
+              <li class="discount-item__detail-item">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary</li>
+            </ul>
+          </li>
+          <li class="discount-item discount-item--true discount__item">
+            <div class="discount-item__header">
+              <h3 class="discount-item__title">Hosting2</h3>
+              <p class="discount-item__days">XXXX Days Retention - U.S. + E.U. Servers</p>
+              <a class="button discount-item__more" href="#">Button</a>
+            </div>
+            <ul class="discount-item__advantages">
+              <li class="discount-item__advantages-item discount-item__advantages-item--price">$x.xx/mo for 1yr</li>
+              <li class="discount-item__advantages-item discount-item__advantages-item--downloads">Unlimited downloads</li>
+              <li class="discount-item__advantages-item discount-item__advantages-item--speed">Unlimited speed</li>
+            </ul>
+            <ul class="discount-item__detail">
+              <li class="discount-item__detail-item">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary</li>
+            </ul>
+          </li>
+          <li class="discount-item discount__item">
+            <div class="discount-item__header">
+              <h3 class="discount-item__title">Hosting3</h3>
+              <p class="discount-item__days">XXXX Days Retention - U.S. + E.U. Servers</p>
+              <a class="button discount-item__more" href="#">Button</a>
+            </div>
+            <ul class="discount-item__advantages">
+              <li class="discount-item__advantages-item discount-item__advantages-item--price">$x.xx/mo for 1yr</li>
+              <li class="discount-item__advantages-item discount-item__advantages-item--downloads">Unlimited downloads</li>
+              <li class="discount-item__advantages-item discount-item__advantages-item--speed">Unlimited speed</li>
+            </ul>
+            <ul class="discount-item__detail">
+              <li class="discount-item__detail-item">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="faq container">
+      <h2 class="faq__title">FAQ’s</h2>
+      <ul class="faq__list">
+        <li class="faq-item faq__item">
+          <h3 class="faq-item__title">Why do we use it?</h3>
+          <div class="faq-item__content">
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+          </div>
+        </li>
+        <li class="faq-item faq__item">
+          <h3 class="faq-item__title">Where can I get some?</h3>
+          <div class="faq-item__content">
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing </p>
+          </div>
+        </li>
+        <li class="faq-item faq__item">
+          <h3 class="faq-item__title">Where does it come from?</h3>
+          <div class="faq-item__content">
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  </main>
+
+  <footer class="page-footer">
+    <div class="container page-footer__grid">
+      <p class="page-footer__copyright">2021 All rights reserved</p>
+      <ul class="footer-menu page-footer__menu">
+        <li class="footer-menu__item">
+          <a class="footer-menu__link" href="#">Privacy Policy</a>
+        </li>
+        <li class="footer-menu__item">
+          <a class="footer-menu__link" href="#">Terms of Service</a>
+        </li>
+        <li class="footer-menu__item">
+          <a class="footer-menu__link" href="#">Copyright Policy</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</body>
+</html>
